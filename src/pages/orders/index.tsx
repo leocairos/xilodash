e37f8d3d57dedf1a8abb495aac1425c76b10a3f9
Leonardo <leocairos@gmail.com>
2021-07-24 14:03:00 -0300

    first commit

--- src/pages/orders/index.tsx
+++ src/pages/orders/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactFlow, {
   ReactFlowProvider,
   addEdge,
@@ -14,11 +14,20 @@ import ReactFlow, {
 } from 'react-flow-renderer';
 import dagre from 'dagre';
 
-import initialElements from './initial-elements';
+const position: XYPosition = { x: 0, y: 0 };
+//import initialElements from './initial-elements';
+const initialElements = [
+  {
+    id: '1',
+    type: 'input',
+    data: { label: 'label' },
+    position,
+  },
+]
 
 import styles from './styles.module.scss';
-
-const position: XYPosition = { x: 0, y: 0 };
+import { api } from '../../services/api'
+import { GetServerSideProps } from 'next';
 
 const dagreGraph = new dagre.graphlib.Graph();
 dagreGraph.setDefaultEdgeLabel(() => ({}));
@@ -28,10 +37,35 @@ const nodeExtent: NodeExtent = [
   [1000, 1000],
 ];
 
-const LayoutFlow = () => {
+interface IOrderSummary {
+  ProdOP: string;
+  CC: string;
+  AnoMes: string;
+  CF: string;
+  TM: string;
+  Produto: string;
+  Descricao: string;
+  Tipo: string;
+  Qtde: number;
+  CustUnit: number;
+  CustoTotal: number;
+  Material: number;
+  MaoDeObra: number;
+  GGF: number;
+}
+
+interface IPropsLayoutFlow {
+  elementsFlow: Elements;
+}
+
+const LayoutFlow = ({elementsFlow}: IPropsLayoutFlow) => {
   const [elements, setElements] = useState<Elements>(initialElements);
   const onConnect = (params: Connection | Edge) => setElements((els) => addEdge(params, els));
   const onElementsRemove = (elementsToRemove: Elements) => setElements((els) => removeElements(elementsToRemove, els));
+
+  useEffect( () => {
+      setElements(elementsFlow)
+  },[])
 
   const onLayout = (direction: string) => {
     const isHorizontal = direction === 'LR';
@@ -63,7 +97,7 @@ const LayoutFlow = () => {
     setElements(layoutedElements);
   };
 
-  const graphStyles = { width: "100%", height: "500px" };
+  const graphStyles = { width: "100%", height: "650px" };
 
   return (
     <div className={styles.layoutflow}>
@@ -88,5 +122,45 @@ const LayoutFlow = () => {
     </div>
   );
 };
+
+export const getServerSideProps: GetServerSideProps = async ( { req, params }) => {
+  const response = await api.get(`/order?anoMes=202105`);
+  const { resumoOPList } = response.data;
+
+  const orders = resumoOPList as IOrderSummary[]
+  const elementsFlow = orders
+    .filter(order => ['PA', 'PI', 'MP'].includes(order.Tipo))
+    .map( item => ({
+      id: item.Produto,
+      data: { label: `${item.Descricao}` },
+      position: { x: 0, y: 0 },
+    }));
+
+  const filteredArr = elementsFlow.reduce((acc, current) => {
+      const x = acc.find(item => item.id === current.id);
+      if (!x) {
+        return acc.concat([current]);
+      } else {
+        return acc;
+      }
+    }, []);
+
+  const edgeFlow = orders
+    .filter(order => ['PA', 'PI', 'MP'].includes(order.Tipo))
+    .map( item => ({
+      id: `${item.Produto} to ${item.ProdOP}`,
+      source: item.Produto,
+      target: item.ProdOP,
+      animated: true,
+      type: 'smoothstep',
+    }));
+
+  return {
+    props: {
+      elementsFlow: [...filteredArr, ...edgeFlow]
+    }
+  }
+}
+
 
 export default LayoutFlow;
